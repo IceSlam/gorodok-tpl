a0cb2d7055ea3cffc25c2bca6f29660c32048ab0
Icelam <mainguy@mail.ru>
2021-02-06 01:03:34 +0300

    Added script and styles for thin navbar

---
+++ assets/js/navscroll.js
@@ -0,0 +1,25 @@
+(function() {
+    'use strict';
+
+    var goTopBtn = document.querySelector('.is-navbar');
+    function trackScroll() {
+        let scrolled = window.pageYOffset;
+        let coords = document.documentElement.clientHeight;
+
+        if (scrolled > coords) {
+            goTopBtn.classList.add('thin');
+        }
+        if (scrolled < coords) {
+            goTopBtn.classList.remove('thin');
+        }
+    }
+
+    function backToTop() {
+        if (window.pageYOffset > 100) {
+            window.scrollBy(0, -40);
+            setTimeout(backToTop, 0);
+        }
+    }
+    window.addEventListener('scroll', trackScroll);
+    goTopBtn.addEventListener('click', backToTop);
+})();
